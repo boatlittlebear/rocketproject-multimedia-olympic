6918fa3212fc02e3429dea12b953e3e75b4a1f34
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-10-05 08:08:13 +0700

    6

--- js/function.js
+++ js/function.js
@@ -3,10 +3,19 @@ var summerHost = [
 ]
 
 var summerDescription = [
-    `1896 เอเธนส์ การแข่งขันโอลิมปิกมีขึ้นที่ยุโรปเป็นครั้งแรก
+    `ครั้งที่ 1<br>
+    เมือง : เอเธนส์ ประเทศ : กรีซ<br>
+    วันที่จัด : 6 - 15 เมษายน 1896<br>
+    จำนวนประเทศที่เข้าร่วม : 13<br>
+    กีฬา : 43 รายการ 9 ชนิด<br>
+    จำนวนนักกีฬา : 311 ชาย 311 หญิง 0`,
+]
+
+var summerHistory = [
+    `การแข่งขันโอลิมปิกมีขึ้นที่ยุโรปเป็นครั้งแรก
     โดยกีฬาโอลิมปิกสมัยใหม่ได้ถูกจัดขึ้นเป็นครั้งแรกในปี 1896
-    หลังจากที่การแข่งขันกีฬาโอลิมปิกได้หยุดไปเป็นเวลานาน
-    โดยจัดขึ้นที่กรุงเอเธนส์ ประเทศกรีซ`,
+    หลังจากที่การแข่งขันกีฬาโอลิมปิกได้หยุดไปเป็นเวลานาน โดยจัดขึ้นที่กรุงเอเธนส์
+    ประเทศกรีซ ที่สนามกีฬา Olympic Stadium ซึ่งได้ถูกทำลายไปโดยทหารฝรั่งเศส`,
 ]
 
 var winterHost = [
@@ -23,14 +32,20 @@ var winterDescription = [
 
 var h = document.getElementById("host");
 var d = document.getElementById("description");
+var s = document.getElementById("story");
 
 function summer() {
-    season1.dataset.season = 'summer';
+    main.dataset.main = 'summer';
+
+    season1.dataset.season = 's-1896';
     season1.dataset.year = 's-1896';
-    h.innerHTML = summerHost[0]
-    d.innerHTML = summerDescription[0]
-    season12.dataset.season = 'summer';
-    season2.dataset.season = 'summer'
+    h.innerHTML = summerHost[0];
+    d.innerHTML = summerDescription[0];
+    season12.dataset.season = 's-1896';
+    season12.dataset.year = 's-1896';
+    s.innerHTML = summerHistory[0];
+
+    season2.dataset.season = 'summer';
     season22.dataset.season = 'summer';
     season3.dataset.season = 'summer';
     season32.dataset.season = 'summer';
@@ -38,13 +53,15 @@ function summer() {
     season42.dataset.season = 'summer';
     season5.dataset.season = 'summer';
     season52.dataset.season = 'summer';
+    footer.dataset.footerseason = 'summer';
 }
 
 function winter() {
+    main.dataset.main = 'winter';
     season1.dataset.season = 'winter';
     season1.dataset.year = 'w-1924';
-    h.innerHTML = winterHost[0]
-    d.innerHTML = winterDescription[0]
+    h.innerHTML = winterHost[0];
+    d.innerHTML = winterDescription[0];
     season12.dataset.season = 'winter';
     season2.dataset.season = 'winter';
     season22.dataset.season = 'winter';
@@ -54,4 +71,15 @@ function winter() {
     season42.dataset.season = 'winter';
     season5.dataset.season = 'winter';
     season52.dataset.season = 'winter';
+    footer.dataset.footerseason = 'winter';
+
 }
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowRight') {
+        athlete.style.backgroundImage = "url('https://media.discordapp.net/attachments/871977129376616459/886628120113844254/image2_1.gif')";
+    }
+    if (event.key === 'ArrowLeft') {
+        athlete.style.backgroundImage = "url('https://media.discordapp.net/attachments/871977129376616459/894723788846030878/ezgif.com-gif-maker.gif')";
+    }
+  });
