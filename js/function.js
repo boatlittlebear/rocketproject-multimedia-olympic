e614e1d78087c98421f567434a5d5fb80b2e322e
IT62070227 <50777893+imlanzelotx@users.noreply.github.com>
2021-10-15 00:05:15 +0700

    view

--- js/function.js
+++ js/function.js
@@ -4,10 +4,10 @@ const output = document.querySelector("#output");
 
 scroller.addEventListener("scroll", event => {
   output.textContent = `scrollLeft: ${scroller.scrollLeft}`;
+  document.body.style.setProperty("--left", scroller.scrollLeft);
 });
 
-
-scroller.addEventListener("scroll", event => {
+scroller.onscroll = function (event) {
 	if (scroller.scrollLeft >= barrier_1.offsetLeft - barrier_1.offsetWidth/2 - window.innerWidth/2) {
 		athlete.style.bottom = "8rem";
 	}
@@ -15,6 +15,18 @@ scroller.addEventListener("scroll", event => {
 		athlete.style.bottom = "1.5rem";
 	}
 	if (scroller.scrollLeft >= barrier_1.offsetLeft + barrier_1.offsetWidth + barrier_1.offsetWidth/2 - window.innerWidth/2) {
+		athlete.style.bottom = "1.5rem";
+	}
+};
+
+scroller.addEventListener('scroll', function (event) {
+	if (event.scrollLeft >= barrier_1.offsetLeft - barrier_1.offsetWidth/2 - window.innerWidth/2) {
+		athlete.style.bottom = "8rem";
+	}
+	else {
+		athlete.style.bottom = "1.5rem";
+	}
+	if (event.scrollLeft >= barrier_1.offsetLeft + barrier_1.offsetWidth + barrier_1.offsetWidth/2 - window.innerWidth/2) {
 		athlete.style.bottom = "1.5rem";
 	}
 });
@@ -36,17 +48,17 @@ scroller.addEventListener("scroll", event => {
 		athlete.style.bottom = "8rem";
 	}
 	else {
-		athlete.style.bottom = "1.5rem";
+		athlete.style.bottom = "2.5rem";
 	}
 	if (scroller.scrollLeft >= barrier_3.offsetLeft + barrier_3.offsetWidth + barrier_3.offsetWidth/2 - window.innerWidth/2 + window.innerWidth * 2) {
-		athlete.style.bottom = "1.5rem";
+		athlete.style.bottom = "2.5rem";
 	}
 });
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowRight') {
         athlete.style.backgroundImage = "url('https://media.discordapp.net/attachments/871977129376616459/886628120113844254/image2_1.gif')";
-    }
+	}
     if (event.key === 'ArrowLeft') {
         athlete.style.backgroundImage = "url('https://media.discordapp.net/attachments/871977129376616459/894723788846030878/ezgif.com-gif-maker.gif')";
     }
